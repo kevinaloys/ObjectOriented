28c3c0efcd299ef89c2f210764cbce641d1f024c
Kevin Aloysius <kevinaloys@rocketmail.com>
2015-01-29 16:15:31 -0800

    Included function to change the temperature

--- Assignment2/src/edu/scu/oop/assign2/Alpha1Oven.java
+++ Assignment2/src/edu/scu/oop/assign2/Alpha1Oven.java
@@ -44,8 +44,15 @@ public class Alpha1Oven
 	
 	public void setTemperature(double degrees)
 	{
-		thermostat.setTemperatureInF((int)degrees);
-		cookingTemp = thermostat.getTemperatureInF();
+		if(degrees <= 72)
+		{
+			System.out.println("Cooking Temperature cannot be below 72\u00b0F");
+		}
+		else
+		{
+			thermostat.setTemperatureInF((int)degrees);
+			cookingTemp = thermostat.getTemperatureInF();
+		}
 	}
 	
 	public void setTime(int cookingTime)
@@ -71,11 +78,13 @@ public class Alpha1Oven
 	
 	public void showOvenStatus()
 	{
+		
 		System.out.println("Oven Name       	 : " + getOvenName());
 		System.out.println("Oven Status     	 : " + status);
-		System.out.println("Start Time      	 : " + startTime);
+		System.out.println("Start Time      	 : " + displayTimeInHumanReadable(startTime));
 		System.out.println("Set Temperature in F 	 : " + thermostat.getTemperatureInF() + "\u00b0" +"F");
-		System.out.println("Stop Time       	 : " + (((long)cookingTimeInMinutes * 60 * 1000) + startTime));
+		long stopTimeInMillis = ((long)cookingTimeInMinutes * 60 * 1000) + startTime;
+		System.out.println("Stop Time       	 : " + displayTimeInHumanReadable(stopTimeInMillis));
 	}
 	
 	private String getOvenName()
@@ -87,7 +96,7 @@ public class Alpha1Oven
 	{
 		if (temp == 0 && time == 0)
 		{
-			System.out.println("Coooking Time and Cooking Temperature are not set");
+			System.out.println("Coooking Time and Cooking Temperature are not set!");
 		}
 		else if(temp == 0)
 		{
@@ -97,5 +106,14 @@ public class Alpha1Oven
 		{
 			System.out.println("Cooking Time is not set!");
 		}
+	}
+	
+	private static String displayTimeInHumanReadable(long millisec)
+	{
+		int seconds = (int) ((millisec % (1000 * 60 * 60)) % (1000 * 60)) / 1000;
+		int minutes = (int) (millisec % (1000 * 60 * 60)) / (1000 * 60);
+		int hours	= (int) ((millisec / (1000 * 60 * 60) ) % 24) - 8;
+		String time = hours + " hours " + minutes + " mintues " + seconds + " seconds";
+		return time;
 	}
 }
