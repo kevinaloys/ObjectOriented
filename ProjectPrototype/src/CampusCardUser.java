a2eda505dd829e135fced748a55a81177ed897bd
Kevin Aloysius <kevinaloys@rocketmail.com>
2015-02-17 23:59:12 -0800

    First Prototype

---
+++ ProjectPrototype/src/CampusCardUser.java
@@ -0,0 +1,27 @@
+public class CampusCardUser
+{
+	private String cardUserName;
+	private String cardUserId;
+	
+	
+	
+	public void setUserName(String name)
+	{
+		cardUserName = name;
+	}
+	
+	public String getUserName()
+	{
+		return cardUserName;
+	}
+	
+	public void setUserId(String id)
+	{
+		cardUserId = id;
+	}
+	
+	public String getUserId()
+	{
+		return cardUserId;
+	}
+}
